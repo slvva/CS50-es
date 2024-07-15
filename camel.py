eac4034000737d6e74d765c974a6f04c3537fa8a
sslv <pignagnolisilvia@gmail.com>
2024-07-15 12:00:07 +0200

    camel.py
    kinda wrong

--- camel.py
+++ camel.py
@@ -3,7 +3,7 @@ camelCase = input("camelCase: ")
 snake_case = []
 for letter in camelCase:
     if letter.isupper():
-        letter = letter.replace(letter, "_" + letter).lower(9
+        letter = letter.replace(letter, "_" + letter).lower()
         snake_case.append(letter)
     else:
         snake_case.append(letter)
