310044c8f4c7124670fa825d9d08588abcc94cff
sslv <pignagnolisilvia@gmail.com>
2024-07-15 09:54:32 +0200

    Create interpreter.py

---
+++ interpreter.py
@@ -0,0 +1,17 @@
+expression = input("expression: ")
+expression = expression.strip()
+
+if expression[2] == "+":
+    result = float(expression[0]) + float(expression[4])
+    print(result)
+elif expression[2] == "-":
+    result = float(expression[0]) - int(expression[4])
+    print(result)
+elif expression[2] == "*":
+    result = float(expression[0]) * float(expression[4])
+    print(result)
+elif expression[2] == "/":
+    result = float(expression[0]) / float(expression[4])
+    print(result)
+else:
+    print("Error")
