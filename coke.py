dc664d18fad3f8788aa2969579afadf1a708773d
sslv <pignagnolisilvia@gmail.com>
2024-07-15 12:30:02 +0200

    Create coke.py

---
+++ coke.py
@@ -0,0 +1,14 @@
+amount = 0
+
+while amount < 50:
+    coin = input("Insert coin(5, 10, 25 cents): ")
+    coin = int(coin)
+    if coin == 5 or coin == 10 or coin == 25:
+        amount += coin
+        if amount < 50:
+            print("Amount Due:", 50 - amount)
+    else:
+        print("Invalid coin")
+
+
+print("Change Owed:", amount - 50)
