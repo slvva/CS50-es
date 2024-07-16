5a0ce95c4147714e7878a92cdff4b7bd422bb60b
sslv <pignagnolisilvia@gmail.com>
2024-07-16 17:01:41 +0200

    Create fuel.py

---
+++ fuel.py
@@ -0,0 +1,19 @@
+while True:
+    str_fraction = input("Fraction: ")
+    try:
+        x, y = str_fraction.split("/")
+        x = int(x)
+        y = int(y)
+        if x <= y:
+            break
+    except (ValueError, ZeroDivisionError):
+        continue
+
+
+percent = 100 * x/y
+if percent <= 1:
+    print("E")
+elif percent >= 99:
+    print("F")
+else:
+    print(f"{percent}%")
