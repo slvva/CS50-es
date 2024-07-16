2a3ad0e9895f8d0c75f7907e539d2be6f58cf097
sslv <pignagnolisilvia@gmail.com>
2024-07-16 12:09:47 +0200

    Create nutrition.py

---
+++ nutrition.py
@@ -0,0 +1,44 @@
+fruit = input("Item: ")
+
+fruit = fruit.casefold()
+
+if fruit == "apple":
+    print("Calories: 130")
+elif fruit == "avocado":
+    print("Calories: 50")
+elif fruit == "banana":
+    print("Calories: 110")
+elif fruit == "cantaloupe":
+    print("Calories: 50")
+elif fruit == "grapefruit":
+    print("Calories: 60")
+elif fruit == "grapes":
+    print("Calories: 90")
+elif fruit == "honeydew melon":
+    print("Calories: 50")
+elif fruit == "kiwifruit":
+    print("Calories: 90")
+elif fruit == "lemon":
+    print("Calories: 15")
+elif fruit == "lime":
+    print("Calories: 20")
+elif fruit == "nectarine":
+    print("Calories: 60")
+elif fruit == "orange":
+    print("Calories: 80")
+elif fruit == "peach":
+    print("Calories: 60")
+elif fruit == "pear":
+    print("Calories: 100")
+elif fruit == "pineaplle":
+    print("Calories: 50")
+elif fruit == "plums":
+    print("Calories: 70")
+elif fruit == "strawberries":
+    print("Calories: 50")
+elif fruit == "sweet cherries":
+    print("Calories: 100")
+elif fruit == "tangerine":
+    print("Calories: 50")
+elif fruit == "watermelon":
+    print("Calories: 80")
